e477f04ea0082db3e8032020bbe73512bb4daae2
achirashah <shah.ac@northeastern.edu>
2020-10-10 00:06:02 -0400

    removed cors

--- webapp-node/app.js
+++ webapp-node/app.js
@@ -1,13 +1,13 @@
 const de =require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const bodyparser = require('body-parser');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(bodyparser.json());
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
+//app.use(cors());
 // 3) ROUTES
 app.use('/v1/user/', userRouter);
 module.exports = app;
